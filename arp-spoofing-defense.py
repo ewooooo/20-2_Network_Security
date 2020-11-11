ef1a0aa2439ce18bc8e8a96024d72ef9e99f247c
ewooooo <nuw577@naver.com>
2020-11-12 04:51:26 +0900

    201112 스푸핑 방어

--- arp-spoofing-defense.py
+++ arp-spoofing-defense.py
@@ -1,7 +1,20 @@
+from psutil import process_iter
+from signal import SIGTERM # or SIGKILL
+
+def kill_port():
+    print("stop telnet server")
+    for proc in process_iter():
+        for conns in proc.connections(kind='inet'):
+            if conns.laddr.port == 23:
+                proc.send_signal(SIGTERM) # or SIGKILL
+                continue
+
 class ArpEntry():
     def __init__(self, line):
         self.ip, self.hw_type, self.flags, self.hw_address, self.mask, self.device = line.split()
-    
+
+    def __str__(self):
+        return '%s: %s' % (self.ip, self.hw_address)
 
 class Spoofing_Defense():
     def __init__(self):
@@ -19,7 +32,10 @@ class Spoofing_Defense():
             try: 
                 count[i.hw_address] += 1
                 if count.get(i.hw_address) > 1:
-                    print("warning spoofing 발생")
+                    print("warning spoofing")
+                    print(self.entries)
+
+                    kill_port()
             except: count[i.hw_address]=1
 
 
